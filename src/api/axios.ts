95e7935e8b8240470bd03fe5ddabcfba308ca0c2
AleksiKlimov <kuzmenkoaleksej59@gmail.com>
2023-01-25 22:23:39 +0300

    add device id in all axios request

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,10 +1,29 @@
 import axios from 'axios';
+import { v4 as uuidv4, v4 } from 'uuid';
 
 import cookies from '../utils/coookieHelper';
 
 const axiosInstance = axios.create({
   baseURL: 'http://localhost:4000/api',
 });
+
+const storageItem = {
+  get() {
+    const data = localStorage.getItem('deviceId');
+    return data;
+  },
+  set(value: string) {
+    localStorage.setItem('deviceId', value);
+  },
+};
+
+const getDeviceId = () => {
+  const deviceId = storageItem.get();
+  if (!deviceId) {
+    return v4();
+  }
+  return deviceId;
+};
 
 axiosInstance.interceptors.request.use((request) => {
   const token = cookies.token.get();
@@ -13,6 +32,7 @@ axiosInstance.interceptors.request.use((request) => {
     customRequest.headers = {
       authorization: `Bearer ${token}`,
       ...request.headers,
+      deviceId: getDeviceId(),
     };
   }
   return request;
