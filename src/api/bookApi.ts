0897a624204558068bb614abf26faddf9bcced0b
AleksiKlimov <leha.kurdyukov@internet.ru>
2023-01-30 17:32:23 +0300

    complete output logic from controller

--- src/api/bookApi.ts
+++ src/api/bookApi.ts
@@ -2,16 +2,22 @@ import type { BookType } from '../store/slices/bookSlice';
 import axiosInstance from './axios';
 
 const getAll = async () => {
-  const response = await axiosInstance.get<{books: BookType[]}>('/book/get-all');
+  const response = await axiosInstance.get<{ books: BookType[] }>('/book/get-all');
   return response.data;
 };
 
 const getById = async (bookId: string | undefined) => {
-  const response = await axiosInstance.get<{book: BookType}>(`/book/${bookId}/book`);
+  const response = await axiosInstance.get<{ book: BookType }>(`/book/${bookId}/book`);
+  return response.data;
+};
+
+const filtered = async (param: string) => {
+  const response = await axiosInstance.get<{ books: BookType[] }>('/book/filtered-books', { params: { param } });
   return response.data;
 };
 
 export default {
   getAll,
   getById,
+  filtered,
 };
